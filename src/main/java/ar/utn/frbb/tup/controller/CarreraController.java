8576d88ac82aad9857ac8e2473025f8872bc8a54
Emanuel Miguel <emanuelmiguel22799@gmail.com>
2024-06-29 21:00:21 -0300

    Modificaciones en controller Carrera

--- src/main/java/ar/utn/frbb/tup/controller/CarreraController.java
+++ src/main/java/ar/utn/frbb/tup/controller/CarreraController.java
@@ -1,16 +1,33 @@
 package ar.utn.frbb.tup.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ar.utn.frbb.tup.business.CarreraService;
+import ar.utn.frbb.tup.business.exception.*;
+import ar.utn.frbb.tup.model.Carrera;
+import ar.utn.frbb.tup.model.dto.CarreraDTO;
+import ar.utn.frbb.tup.persistence.exception.NoEncontradoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("carrera")
 public class CarreraController {
+    @Autowired
+    CarreraService carreraService;
+
 
     @PostMapping("/")
-    public String crearCarrera () {
-        return "safa";
+    public Carrera crearCarrera(@RequestBody CarreraDTO carreraDTO) throws NombreInvalidoException, CantidadCuatrimestresInvalidaException, DatoNumericoInvalidoException {
+        return carreraService.crearCarrera(carreraDTO);
+    }
+
+    @DeleteMapping("/{codigoCarrera}")
+    public String eliminarCarrera(@PathVariable Integer codigoCarrera) throws NoEncontradoException {
+        return carreraService.eliminarCarrera(codigoCarrera);
+    }
+
+    @PutMapping("/{codigoCarrera}")
+    public Carrera actualizarCarrera(@PathVariable Integer codigoCarrera, @RequestBody CarreraDTO carreraDTO) throws CantidadCuatrimestresInvalidaException, NombreInvalidoException, NoEncontradoException, DatoNumericoInvalidoException {
+        return carreraService.actualizarCarrera(codigoCarrera, carreraDTO);
     }
 
 }
--- src/main/java/ar/utn/frbb/tup/controller/handler/ResponseExceptionHandler.java
+++ src/main/java/ar/utn/frbb/tup/controller/handler/ResponseExceptionHandler.java
@@ -1,2 +1,66 @@
-package ar.utn.frbb.tup.controller.handler;public class ResponseExceptionHandler {
+package ar.utn.frbb.tup.controller.handler;
+
+import ar.utn.frbb.tup.business.exception.*;
+import ar.utn.frbb.tup.persistence.exception.NoEncontradoException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {NombreInvalidoException.class})
+    protected ResponseEntity<Object> handleNombreInvalidoException(NombreInvalidoException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {CantidadCuatrimestresInvalidaException.class})
+    protected ResponseEntity<Object> handleCantidadCuatrimestresInvalidaException(CantidadCuatrimestresInvalidaException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {DatoNumericoInvalidoException.class})
+    protected ResponseEntity<Object> handleDatoNumericoInvalidoException(DatoNumericoInvalidoException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+    @ExceptionHandler(value = {NoEncontradoException.class})
+    protected ResponseEntity<Object> handleNoEncontradoException(NoEncontradoException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value
+            = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (body == null) {
+            CustomApiError error = new CustomApiError();
+            error.setErrorMessage(ex.getMessage());
+            body = error;
+        }
+
+        return new ResponseEntity(body, headers, status);
+    }
 }
